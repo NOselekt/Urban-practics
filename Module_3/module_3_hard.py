94b325cdf9323c0e13fe84d9c78d224f047f3780
NOselekt <139881292+NOselekt@users.noreply.github.com>
2024-07-24 17:25:06 +0500

    Version 1.0

---
+++ Module_3/module_3_hard.py
@@ -0,0 +1,33 @@
+data_structure = [
+  [1, 2, 3],
+  {'a': 4, 'b': 5},
+  (6, {'cube': 7, 'drum': 8}),
+  "Hello",
+  ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+def struct_count(arg=None):
+    struct = []
+    res = 0
+    if isinstance(arg, dict):
+        struct = [[i, j] for i, j in arg.items()]
+    else:
+        struct = [i for i in arg]
+    if struct:
+        for i in struct:
+            if isinstance(i, dict):
+                for j, k in i.items():
+                    res += len(j)
+                    if isinstance(k, int) or isinstance(k, float):
+                        res += k
+                    elif isinstance(k, str):
+                        res += len(k)
+            elif isinstance(i, int) or isinstance(i, float):
+                res += i
+            elif isinstance(i, str):
+                res += len(i)
+            else:
+                res += struct_count(i)
+    return res
+
+print(struct_count(data_structure))
