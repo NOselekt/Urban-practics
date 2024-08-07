3f2aa464c82f091c16085898db125e093b5e99d6
NOselekt <139881292+NOselekt@users.noreply.github.com>
2024-08-08 00:23:09 +0500

    Done

---
+++ Module_7/module_7_2/module_7_2.py
@@ -0,0 +1,31 @@
+def custom_write(file_name: str, strings: list):
+    file = open(file_name, 'r', encoding='utf-8')
+    file_viscera = file.read()
+    file.close()
+
+    file = open(file_name, 'a', encoding='utf-8')
+    if file_viscera:
+        file.write('\n')
+        file_viscera += '\n'
+
+    string_positions = {}
+
+    for string in strings:
+        string_positions[(file_viscera.count('\n') + 1, file.tell())] = string
+        file_viscera += string + '\n'
+        file.write(string + '\n')
+
+    return string_positions
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+  print(elem)
+
+
