795e30c0c34881bb7c17aba974e581bdd87d13aa
NOselekt <139881292+NOselekt@users.noreply.github.com>
2024-08-15 10:49:44 +0500

    Update module_8_1.py

--- Module_8/module_8_1.py
+++ Module_8/module_8_1.py
@@ -16,32 +16,3 @@ def add_everything_up(*summands):
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))
-
-'''
-def add_everything_up(*summands):
-    if isinstance(summands[0], int):
-        result = 0
-    else:
-        result = ''
-
-    length = len(summands)
-
-    try:
-        for i in range(length):
-            result += summands[i]
-    except TypeError:
-        result = str(result)
-        for j in range(i, length):
-            result += str(summands[j])
-
-    return result
-
-Лучше оптимизация (?) XD, но не намного
-При двух миллионах простеньких аргументов разница - 3 секунды)
-'''
-
-
-
-print(add_everything_up(123.456, 'строка'))
-print(add_everything_up('яблоко', 4215))
-print(add_everything_up(123.456, 7))
