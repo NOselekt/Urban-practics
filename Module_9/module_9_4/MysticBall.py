606119aafc0f976f9900e64ad2cff8d7a0307dee
NOselekt <139881292+NOselekt@users.noreply.github.com>
2024-08-21 21:44:48 +0500

    Updated

--- Module_9/module_9_4/MysticBall.py
+++ Module_9/module_9_4/MysticBall.py
@@ -1,16 +1,19 @@
 import random
-from typing import TypeVar
-Collections = TypeVar('A', list, tuple, set)
-
 
 class MysticBall:
-    def __init__(self, *words: Collections):
-        self.words = tuple([element for element in words])
+    def __init__(self, *words: str):
+        if any(not isinstance(element) for element in words):
+            raise TypeError
+        else:
+            self.words = tuple([element for element in words])
 
     def __call__(self):
         return random.choice(self.words)
 
-first_ball = MysticBall('Да', 'Нет', 'Наверное')
-print(first_ball())
-print(first_ball())
-print(first_ball())
+try:
+    first_ball = MysticBall('Да', 'Нет', 'Наверное')
+    print(first_ball())
+    print(first_ball())
+    print(first_ball())
+except TypeError:
+    print('Введённые данные неверного типа, живите с этим')
--- Module_9/module_9_4/closure.py
+++ Module_9/module_9_4/closure.py
@@ -1,10 +1,13 @@
-import pickle
 
 def get_advanced_writer(file_name: str):
     def write_everything(*data_set):
         with open(file_name, 'a') as file:
-            print(*data_set, file=file, sep='')
+            print(*data_set, file=file, sep='\n')
     return write_everything
 
-write = get_advanced_writer('example.txt')
-write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+try:
+    write = get_advanced_writer('example.txt')
+    write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+except TypeError:
+    print('Введённые данные неверного типа, живите с этим')
