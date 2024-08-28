fa28d4c107e2e5665568c254e106b83d50916eda
NOselekt <139881292+NOselekt@users.noreply.github.com>
2024-08-28 16:49:13 +0300

    Done

--- Module_10/module_10_1/module_10_1.py
+++ Module_10/module_10_1/module_10_1.py
@@ -1,13 +1,14 @@
 import threading
 from declarations import write_words, ReturningThread
 
-FILES_NAMES = {'example1.txt': 10, 'example2.txt': 30, 'example3.txt': 200, 'example4.txt': 100}
+FILES_NAMES_1 = {'example1.txt': 10, 'example2.txt': 30, 'example3.txt': 200, 'example4.txt': 100}
+FILES_NAMES_2 = {'example5.txt': 10, 'example6.txt': 30, 'example7.txt': 200, 'example8.txt': 100}
 
 if __name__ =='__main__':
 
 #в один поток
 
-    singlethread_time = sum(write_words(FILES_NAMES[name], name) for name in FILES_NAMES)
+    singlethread_time = sum(write_words(FILES_NAMES_1[name], name) for name in FILES_NAMES_1)
 
     print(f'Однопоточная программа работала: {round(singlethread_time, 4)} секунды')
 
@@ -15,10 +16,10 @@ if __name__ =='__main__':
 
     threads = {}
 
-    for name in FILES_NAMES:
-        threads[name] = ReturningThread(target=write_words, args=(FILES_NAMES[name], name))
+    for name in FILES_NAMES_2:
+        threads[name] = ReturningThread(target=write_words, args=(FILES_NAMES_2[name], name))
         threads[name].start()
-    multithread_time = sum(threads[name].join() for name in FILES_NAMES)
+    multithread_time = sum(threads[name].join() for name in FILES_NAMES_2)
 
 
     print(f'Однопоточная программа работала: {round(singlethread_time, 4)} секунды')
