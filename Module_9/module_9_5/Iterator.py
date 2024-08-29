6a97ea22e80f61f5b22ab287a907f9dab8c67553
NOselekt <139881292+NOselekt@users.noreply.github.com>
2024-08-29 20:24:16 +0300

    Updated

--- Module_9/module_9_5/Iterator.py
+++ Module_9/module_9_5/Iterator.py
@@ -3,7 +3,7 @@ class StepValueError(ValueError):
 
 
 class Iterator:
-    def __init__(self, start: int, stop: int, step: int =1):
+    def __init__(self, start: int, stop: int, step: int = 1):
         if step == 0:
             raise StepValueError('Шаг не может быть равен 0')
         self.start = start
@@ -16,7 +16,7 @@ class Iterator:
         return self
 
     def __next__(self):
-        if (self.step > 0 and self.pointer >= self.stop) or (self.step < 0 and self.pointer <= self.stop):
-            raise StopIteration
         self.pointer += self.step
+        if (self.step > 0 and self.pointer > self.stop) or (self.step < 0 and self.pointer < self.stop):
+            raise StopIteration
         return self.pointer
