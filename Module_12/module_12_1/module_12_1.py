c74eab640fea78a052da8bc5ef58a53059a1c7c9
NOselekt <139881292+NOselekt@users.noreply.github.com>
2024-09-09 11:16:10 +0300

    Done

---
+++ Module_12/module_12_1/module_12_1.py
@@ -0,0 +1,27 @@
+import unittest
+from runner import Runner
+
+
+class TestRunner(unittest.TestCase):
+    def test_walk(self):
+        test_runner = Runner('Ahmed')
+        for _ in range(10):
+            test_runner.walk()
+        self.assertEqual(test_runner.distance, 50)
+
+    def test_run(self):
+        test_runner = Runner('Ahmed')
+        for _ in range(10):
+            test_runner.run()
+        self.assertEqual(test_runner.distance, 100)
+
+    def test_challenge(self):
+        test_runner_1 = Runner('Ahmed')
+        test_runner_2 = Runner('Mahmud')
+        for _ in range(10):
+            test_runner_1.run()
+            test_runner_2.walk()
+        self.assertNotEqual(test_runner_1.distance, test_runner_2.distance)
+
+if __name__ == '__main__':
+    unittest.main()
