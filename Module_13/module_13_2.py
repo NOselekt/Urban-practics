2cdb4bf8b7a4e94065459a10f829815f2c991541
NOselekt <139881292+NOselekt@users.noreply.github.com>
2024-09-13 15:54:58 +0300

    Done

---
+++ Module_13/module_13_2.py
@@ -0,0 +1,22 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from pprint import pprint
+import json
+import asyncio
+
+TOKEN = 'place your token here'
+BOT = Bot(token=TOKEN)
+DISPATCHER = Dispatcher(bot=BOT, storage=MemoryStorage())
+
+
+@DISPATCHER.message_handler(commands=['start'])
+async def start_message(message):
+    print('Привет! Я бот, помогающий твоему здоровью.')
+
+@DISPATCHER.message_handler()
+async def all_messages(message):
+    print('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(DISPATCHER, skip_updates=True)
