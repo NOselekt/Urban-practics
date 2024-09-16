6004ea4d269eb903faa47285353474192548df9d
NOselekt <139881292+NOselekt@users.noreply.github.com>
2024-09-16 17:35:42 +0300

    Done

--- Module_14/module_14_1/module_14_1.py
+++ Module_14/module_14_1/module_14_1.py
@@ -19,9 +19,9 @@ cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
 for i in range(1, 11):
     cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
                    (f"User{i}", f"example{i}@gmail.com", f"{10*i}", "1000"))
-for i in range(1, 10, 2):
+for i in range(1, 11, 2):
     cursor.execute("UPDATE Users SET balance = ? WHERE username = ?", ("500", f"User{i}"))
-for i in range(1, 10, 3):
+for i in range(1, 11, 3):
     cursor.execute("DELETE FROM Users WHERE username = ?", (f"User{i}", ))
 
 cursor.execute("SELECT username, email, age, balance FROM Users  WHERE age != ?", ("60", ))
