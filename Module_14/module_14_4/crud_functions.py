e426891d4b8f7eb84bfd238a2d34b7d6cc3267bc
NOselekt <139881292+NOselekt@users.noreply.github.com>
2024-09-19 11:03:27 +0300

    Update

--- Module_14/module_14_4/crud_functions.py
+++ Module_14/module_14_4/crud_functions.py
@@ -1,11 +1,11 @@
 import sqlite3
 import pprint
 
+with sqlite3.connect("initiate.db") as connection:
+    cursor = connection.cursor()
 
-def initiate_db():
-    with sqlite3.connect("initiate.db") as connection:
-        cursor = connection.cursor()
 
+    def initiate_db():
         cursor.execute('''
         CREATE TABLE IF NOT EXISTS Products(
             id INTEGER PRIMARY KEY,
@@ -16,19 +16,21 @@ def initiate_db():
         )''')
 
 
-def get_all_products():
-    with sqlite3.connect("initiate.db") as connection:
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM Products")
-        data = cursor.fetchall()
-    return data
-
-
-if __name__ == "__main__":
-    initiate_db()
-    with sqlite3.connect("initiate.db") as connection:
-        cursor = connection.cursor()
+    def fill_db():
         for i in range(1, 11):
             cursor.execute("INSERT INTO Products (title, description, price, image_path) VALUES (?, ?, ?, ?)",
                            (f"Название: Продукт{i}", f"Описание: {i * 100} г", i * 100, f"images/{i}.jpg"))
-    pprint.pprint(get_all_products())
+
+
+    def get_all_products():
+        cursor.execute("SELECT * FROM Products")
+        data = cursor.fetchall()
+        return data
+
+
+    if __name__ == "__main__":
+        initiate_db()
+        data = get_all_products()
+        if not data:
+            fill_db()
+        pprint.pprint(data)
