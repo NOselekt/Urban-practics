b980a8a969e0958c87d1129ccc680de204fac34c
NOselekt <139881292+NOselekt@users.noreply.github.com>
2024-10-01 16:18:21 +0300

    Update

--- Module_16/module_16_1.py
+++ Module_16/module_16_1.py
@@ -7,13 +7,13 @@ async def welcome() -> str:
     return "Main page"
 
 @app.get("/user/admin")
-async def welcome() -> str:
+async def welcome_admin() -> str:
     return "You logged in as an administrator"
 
 @app.get("/user/{id_}")
-async def welcome(id_ = 0) -> str:
+async def welcome_user(id_ = 0) -> str:
     return f"You logged in as user №{id_}"
 
 @app.get("/user/")
-async def welcome(username: str = "admin", age: int = 23) -> str:
+async def user_info(username: str = "admin", age: int = 23) -> str:
     return f"User's inforamtion: username: {username}, age: {age}"
