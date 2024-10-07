35a3ec4ca20b693f34de73729b8a36b2162b3ea8
NOselekt <139881292+NOselekt@users.noreply.github.com>
2024-10-07 14:00:05 +0300

    Update

--- Module_16/module_16_3.py
+++ Module_16/module_16_3.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, HTTPException
 
 app = FastAPI()
 
-users = {'1': 'Имя: Example, возраст: 18'}
+users = {1: 'Имя: Example, возраст: 18'}
 
 @app.get("/")
 async def welcome() -> str:
@@ -13,20 +13,26 @@ async def get_dict() -> dict:
     return users
 
 @app.post("/user/{username}/{age}")
-async def add_user(username: str = Path(min_length=5, max_length=20, description="Enter username", example="admin"),
+async def add_user(username: str = Path(min_length=5, max_length=20, description="Enter username", example="executor"),
                    age: int = Path(ge=18, le=120, description="Enter age", example="23")) -> str:
-    last_index = max(users, key=int)
+    try:
+        last_index = max(users, key=int)
+    except ValueError:
+        last_index = 1
     users[last_index] = f"Имя: {username}, возраст: {age}"
     return f"User №{last_index} is registered"
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def update_user(id_: str = Path(ge=1, le=100, description="Enter User ID", example=1),
-                      username: int = Path(min_length=5, max_length=20, description="Enter username", example="admin"),
-                      age: int = Path(ge=18, le=120, description="Enter age", example="23")) -> str:
-    users[id_] = f"Имя: {username}, возраст: {age}"
-    return f"The user №{id_} is registered"
+async def update_user(user_id: int = Path(gt=1, le=100, description="Enter User ID", example=2),
+                      username: str = Path(min_length=5, max_length=20, description="Enter username", example="executor"),
+                      age: int = Path(ge=18, le=120, description="Enter age", example=23)) -> str:
+    users[user_id] = f"Имя: {username}, возраст: {age}"
+    return f"The user №{user_id} is registered"
 
 @app.delete("/user/{user_id}")
-async def delete_user(id_: str = Path(ge=1, le=100, description="Enter User ID", example=1)) -> str:
-    users.pop(id_)
-    return f"The user №{id_} is deleted"
+async def delete_user(user_id: int = Path(ge=1, le=100, description="Enter User ID", example=2)) -> str:
+    try:
+        users.pop(user_id)
+        return f"The user №{user_id} is deleted"
+    except IndexError:
+        raise HTTPException(status_code=404, detail="The dictionary's already empty")
